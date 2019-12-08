8710e7e3480b77b4ccef07eb3a43e8ac2db66388
lastfreeacc <last.free.acc@gmail.com>
2019-12-09 00:30:51 +0300

    expand partitions

--- kafka/scripts/entry.sh
+++ kafka/scripts/entry.sh
@@ -26,7 +26,7 @@ echo "creating new kafka topic"
 $KAFKA_HOME/bin/kafka-topics.sh --create \
     --zookeeper localhost:2181 \
     --replication-factor 1 \
-    --partitions 1 \
+    --partitions 2 \
     --topic logs
 
 echo "topic created"
