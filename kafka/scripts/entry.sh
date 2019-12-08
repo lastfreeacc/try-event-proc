dda02d42c1a97e7c6c812ecbb0c138fe7aee15a7
lastfreeacc <last.free.acc@gmail.com>
2019-12-08 15:16:00 +0300

    create logs topic

--- kafka/scripts/entry.sh
+++ kafka/scripts/entry.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # run zookeeper
+echo "starting zookeeper"
 $ZK_HOME/bin/zkServer.sh start
 
 until $ZK_HOME/bin/zkServer.sh status; do
@@ -9,7 +10,29 @@ done
 
 echo "zookeeper started"
 
-# run Kafka
-$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
+# run Kafka as demon
+echo "starting kafka"
+$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties >/dev/null 2>&1 < /dev/null &
+KAFKA_PID=$!
 
-echo "kafka started"
+echo "kafka will start soon"
+
+while ! nc -z localhost 9092; do   
+  sleep 1
+done
+
+echo "ready for messaging"
+echo "creating new kafka topic"
+$KAFKA_HOME/bin/kafka-topics.sh --create \
+    --zookeeper localhost:2181 \
+    --replication-factor 1 \
+    --partitions 1 \
+    --topic logs
+
+echo "topic created"
+$KAFKA_HOME/bin/kafka-topics.sh --list \
+    --zookeeper localhost:2181
+
+echo "ready for business..."
+
+wait "${KAFKA_PID}"
